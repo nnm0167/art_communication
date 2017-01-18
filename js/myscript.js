31a4623c752120eacc6cdf3cf2f42a1c33d981a1
nnm0167 <nnm.bnn0167@gmail.com>
2017-01-18 23:58:56 +0900

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,105 @@
+$(function () {
+  $('a[href^=#]').click(function () {
+    var href = $(this).attr("href"),
+        target = $(href === "#" || href === "" ? 'html' : href);
+    target.velocity("scroll", { duration: 1200, easing: "ease" });
+    return false;
+  });
+});
+
+
+/*boxer*/
+$(function(){
+$(".boxer").boxer();
+});
+
+/*logo*/
+$('.logo').mouseenter(function() {
+    $(this).velocity({
+        opacity: 0 },
+      {
+      duration: 200,
+      visibility: "hidden"
+    });
+    $('.logo_hidden').velocity({
+      opacity: 1 },
+    {
+      duration: 200,
+      visibility: "visible"
+    });
+});
+$('.logo_hidden').mouseleave(function() {
+    $(this).velocity({
+      opacity: 0 },
+    {
+      duration: 200,
+      visibility: "hidden"
+    });
+    $('.logo').velocity({
+      opacity: 1 },
+    {
+      duration: 200,
+      visibility: "visible"
+    });
+});
+
+
+/*flexbox*/
+var hsize = 0;
+
+var itemShow = {
+  p: {
+    opacity: [1, 0]
+  },
+  o: {
+    display: "flex",
+    visibility: "visible",
+    duration: 1300,
+    easing: 'easeIn',
+    begin: function() {
+      $("#position").css( "display" , "block");
+    }
+  }
+};
+
+var itemHide = {
+  p: {
+    opacity: [0, 1]
+  },
+  o: {
+    display: "none",
+    visibility: "hidden",
+    duration: 300,
+    easing: 'easeOut'
+  }
+};
+
+$(".a").on('click',function() {
+  $(".box2").velocity(itemHide);
+  $(".box3").velocity(itemHide);
+  $(".box1").velocity(itemShow);
+  $(this).prop("disabled", true);
+  $(".b").prop("disabled", false);
+  $(".c").prop("disabled", false);
+  });
+$(".b").on('click',function() {
+  $(".box1").velocity(itemHide);
+  $(".box3").velocity(itemHide);
+  $(".box2").velocity(itemShow);
+  $(this).prop("disabled", true);
+  $(".a").prop("disabled", false);
+  $(".c").prop("disabled", false);
+});
+$(".c").on('click',function() {
+  $(".box1").velocity(itemHide);
+  $(".box2").velocity(itemHide);
+  $(".box3").velocity(itemShow);
+  $(this).prop("disabled", true);
+  $(".a").prop("disabled", false);
+  $(".b").prop("disabled", false);
+});
+
+$('.logo_end').click(function() {
+  $('header')
+    .velocity("scroll", { duration: 1000, easing: "easeIn" });
+});
